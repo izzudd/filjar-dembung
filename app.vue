fc7c871e0853ff339a8a9e2be705f77cad84b139
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2022-08-19 15:11:16 +0700

    setup color and typography

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="h-screen w-screen flex items-center justify-center">
-    <h1 class="font-xl">Hello World</h1>
+    <Head>
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin="anonymous"
+      />
+      <Link
+        href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Open+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <div class="flex flex-col gap-4">
+      <h1>Hello World</h1>
+      <h2>Lorem ipsum</h2>
+      <h3>This is H3</h3>
+      <p>This is paragraph</p>
+    </div>
   </div>
 </template>
+
+<style lang="postcss">
+html {
+  @apply font-body text-accent-black/80;
+}
+
+h1,
+h2 {
+  @apply font-heading;
+}
+
+h1 {
+  @apply text-6xl text-accent-black;
+}
+
+h2 {
+  @apply text-5xl text-primary;
+}
+
+h3 {
+  @apply text-xl font-semibold;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,27 @@ module.exports = {
     './pages/**/*.{vue,js,ts,tsx}',
     './plugins/**/*.{js,ts}',
   ],
+
   theme: {
     extend: {
       fontFamily: {
-        // TODO: include font fot heading and body
         // Heading font Fugaz One fallback to serif
+        heading: ['Fugaz One', defaultTheme.fontFamily.serif],
         // Body font Open Sans fallback to sans
+        body: ['Open Sans', defaultTheme.fontFamily.sans],
+      },
+
+      // design system color pallete
+      colors: {
+        primary: '#295C56',
+        secondary: '#D1E8DC',
+        accent: '#9BB0A5',
+        'accent-white': '#F9F8F3',
+        'accent-black': '#0C120C',
       },
     },
   },
+
   // TODO: decide use typography fom plugin or manual
   // plugins: [require('@tailwindcss/typography')],
 };
