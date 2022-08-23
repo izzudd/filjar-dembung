7b9edafcccdab951ea57377cadb24360c5584bc2
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2022-08-23 22:51:08 +0700

    switch to `@nuxtjs/tailwindcss`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,8 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/tw.css'],
-  
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          'tailwindcss/nesting': {},
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-  },
-})
+
+  modules: ['@nuxtjs/tailwindcss'],
+});
