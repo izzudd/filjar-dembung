7014fdd24c82a171a1e6378350755b1d862bbe29
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2022-08-23 21:11:24 +0700

    add video overlay

--- components/landing/LandingHero.vue
+++ components/landing/LandingHero.vue
@@ -1,18 +1,34 @@
 <template>
   <SectionWrapper class="cover">
-    <div class="min-h-screen flex items-center justify-center mt-12">
-      <div class="text-center max-w-3xl">
-        <h1 class="mb-4">D'EMBUNG PARK PANDANMULYO</h1>
-        <p class="text-xl">Tempat Wisata Terbaik, Untuk Keluarga Tercinta</p>
-        <button>
-          <!-- TODO: Implement onclick event -->
-          <IconPlay class="fill-accent-white hover:fill-[#D6D8CE] mt-6" />
-        </button>
+    <div class="relative">
+      <div class="min-h-screen flex items-center justify-center mt-12">
+        <div class="text-center max-w-3xl">
+          <h1 class="mb-4">D'EMBUNG PARK PANDANMULYO</h1>
+          <p class="text-xl">Tempat Wisata Terbaik, Untuk Keluarga Tercinta</p>
+          <button @click="overlay = true">
+            <!-- TODO: Implement onclick event -->
+            <IconPlay class="fill-accent-white hover:fill-[#D6D8CE] mt-6" />
+          </button>
+        </div>
+      </div>
+      <div
+        v-show="overlay"
+        class="bg-accent-black/20 fixed top-0 left-0 w-screen h-screen p-12 z-50"
+      >
+        <div class="w-full h-full bg-slate-200 relative">
+          <button class="top-5 left-5 absolute" @click="overlay = false">
+            <IconClose class="stroke-accent-black" />
+          </button>
+          <!-- TODO: Add video -->
+        </div>
       </div>
     </div>
-    <!-- TODO: Add video overlay -->
   </SectionWrapper>
 </template>
+
+<script setup lang="ts">
+const overlay = ref(false);
+</script>
 
 <style lang="postcss" scoped>
 .cover {
