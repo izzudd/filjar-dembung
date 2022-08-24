efa6bd0c970a93cf6077c9b2fa3879105d7ea44b
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2022-08-24 13:58:24 +0700

    add searchbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,16 +2,18 @@
   <div class="w-full px-4 shadow-md fixed top-0 left-0 z-40 bg-accent-white">
     <header class="container mx-auto flex justify-between py-4">
       <!-- Logo -->
-      <img src="/img/logo.svg" alt="D'EMBUNG PARK" />
+      <NuxtLink to="/"
+        ><img src="/img/logo.svg" alt="D'EMBUNG PARK"
+      /></NuxtLink>
 
       <!-- Nav menu and CTA -->
       <div class="flex items-center font-semibold">
         <!-- TODO: Add link and onclick event -->
         <nav class="flex gap-8">
-          <NuxtLink to="#">Fasilitas</NuxtLink>
-          <NuxtLink to="#">Lokasi</NuxtLink>
-          <NuxtLink to="#">Testimoni</NuxtLink>
-          <NuxtLink to="#">Artikel</NuxtLink>
+          <NuxtLink to="/#fasilitas">Fasilitas</NuxtLink>
+          <NuxtLink to="/#lokasi">Lokasi</NuxtLink>
+          <NuxtLink to="/#testimoni">Testimoni</NuxtLink>
+          <NuxtLink to="/artikel">Artikel</NuxtLink>
         </nav>
         <ButtonAction class="primary ml-8"> Hubungi Kami </ButtonAction>
       </div>
---
+++ components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex bg-secondary p-2">
+    <input
+      v-model="query"
+      type="text"
+      placeholder="Cari sesuatu?"
+      class="flex-shrink bg-transparent mr-2"
+      @keydown.self.enter="
+        navigateTo({
+          path: '/artikel',
+          query: {
+            s: query,
+          },
+        })
+      "
+    />
+    <NuxtLink
+      :to="{
+        path: '/artikel',
+        query: {
+          s: query,
+        },
+      }"
+    >
+      <img src="/icons/search.svg" alt="cari"
+    /></NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const query = ref('');
+</script>
