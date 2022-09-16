03b874a0d9e48f781ea60978a61e990b9495851e
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2022-09-16 22:01:12 +0700

    setup mock data

---
+++ mock/articleSingle.js
@@ -0,0 +1,41 @@
+module.exports = (id) => ({
+  id,
+  slug: 'content-mock-' + id,
+  title: 'Cara Mudah Budidaya Ikan Nila Agar Cepat Panen',
+  image: 'https://source.unsplash.com/random/1920x1080/?wallpaper,landscape',
+  createdAt: 'August 23, 2022 10:58 AM',
+  updatedAt: 'August 23, 2022 11:03 AM',
+  body: content,
+});
+
+const content = `Penulis : Dinas Ketahanan Pangan dan Perikanan
+
+Sumber : [https://dkpp.bulelengkab.go.id/informasi/detail/artikel/cara-mudah-budidaya-ikan-nila-agar-cepat-panen-26](https://dkpp.bulelengkab.go.id/informasi/detail/artikel/cara-mudah-budidaya-ikan-nila-agar-cepat-panen-26)
+
+Sumber Foto : [moondoggiesmusic.com](http://moondoggiesmusic.com/)
+
+**Ternak Ikan Nila** – Budidaya ikan nila merupakan sebuah budidaya yang sudah tidak asing lagi di negara kita Indonesia. Hampir diseluruh daerah pasti ada yang membudidayakan jenis ikan ini. Karena ikan nila merupakan ikan yang memiliki pertumbuhan dan perkembangbiakan yang paling cepat daripada ikan lainnya. Dikatakan pertumbuhan dan perkembangannya sangat cepat, karena para peternak ikan sudah merasakannya sendiri saat membudidayakan ikan nila ini. Sehingga panen untuk ikan nila ini tidak memakan waktu yang sangat lama.Cara pembudidayaan ikan nila ini bisa menggunakan berbagai cara, seperti membudidayakan dengan tambak, kolam semen, kolam terpal, keramba, jaring apung, bahkan bisa di sawah dan di air yang payau ikan nila ini bisa tumbuh dan berkembangbiak dengan baik.Pertumbuhan ikan nila bisa mencapai berat 1 kg per ekornya. Ikan nila memiliki tekstur daging yang enak. Oleh karena itu ikan ini merupakan ikan favorit bagi para peternak serta para pembelinya untuk dikonsumsi. Selain itu harga jual ikan nila ini di pasaran cukup tinggi.Nah sekarang bagaimana cara memulai budidaya ikan nila? Di bawah ini akan dijelaskan langkah-langkah yang tepat untuk memulai budidaya ikan nila.Kolam yang biasanya digunakan sebagai pembudidayaan ikan nila seperti yang sudah disinggung tadi, sangat-sangat fleksibel. Namun yang paling banyak digunakan untuk membudidayakan ikan nila biasanya menggunakan kolam yang terbuat dari tanah atau lebih tepatnya menggali tanah untuk dijadikan sebagai kolam.
+
+Perlu diketahui bahwa, tidak hanya ikan nila saja yang dibudidaya menggunakan kolam yang terbuat dari tanah, melainkan hampir semua jenis ikan air tawar dibudidayakan di kolam tanah. Seperti ikan lele, ikan gurame, ikan mujair, dan lain sebagainya.Nah, lalu kenapa lebih banyak yang menggunakan kolam tanah untuk budidaya? Hal ini karena biaya untuk konstruksi pembuatan kolam dari tanah ini cukup murah dan proses pembuatannya juga lebih mudah dibandingkan yang lainnya.Dan kelebihan-kelebihan yang diberikan oleh kolam dari tanah ini lebih banyak. Seperti terdapat hewan-hewan yang hidup yang juga sangat bermanfaat untuk pakan ikan nila, selain itu juga tumbuh berbagai tumbuhan yang bisa dijadikan pakan ternak ikan nila tersebut.Selain itu dengan kolam pembudidayaan ikan nila yang terbuat dari tanah biasanya tumbuh lebih sehat dan cepat. Oleh karena itu pembudidaya ikan yang menggunakan kolam dari tanah tidak perlu banyak-banyak untuk membeli pakan ternak ikan nila.
+
+Hal yang sangat penting dan perlu diperhatikan adalah pada saat pemilihan benih ikan nila. Kenapa? Karena benih ikan nila yang bagus, juga merupakan sebuah faktor yang menentukan keberhasilan dalam pembudidayaan ikan nila nantinya.Nah dalam memilih benih ikan nila yang lainnya juga pilih benih ikan nila yang berjenis kelamin laki-laki untuk mempunyai hasil yang maksimal, karena pertumbuhan ikan nila jantan lebih cepat 40% dari pada ikan nila yang berjenis kelamin betina.Ikan nila memiliki sifat memijah atau melakukan perkawinan. Jadi membudidayakan ikan nila satu jenis kelamin lebih produktif dari pada yang dicampur. Karena jika dibudidayakan bercampur, ikan nila akan kehabisan energy untuk melakukan perkawinan dan bobot ikan sedikit terhambat.
+
+Umumnya ukuran air yang siap untuk digunakan dalam membididayakan ikan nila terisi air sedalam 60 sampai 75 cm. padat tebar kolam dari tanah untuk membudidayakan ikan nila sebanyak 15 sampai 30 ekor per meter persegi.Dan ukuran ikan benih sebesar 10 sampai 20 gram per ekor. Dan standarnya untuk pemanenan biasanya ukuran ikan nila sudah mencapai lebih dari 300 gram per ekor.Sebelum benih ikan nila ditebar, biasanya benih melakukan tahap pembiasaan adaptasi terlebih dahulu terhadap kondisi kolam. Supaya untuk mengurangi resiko kematian benih ikan nanti saat diterjukan atau ditebarkan ke dalam kolam.
+
+### Pemeliharaan Budidaya Ikan Nila
+
+Setelah semua persiapan selesai dilakukan dan benih sudah ditebarkan ke dalam kolam, langkah selanjutnya adalah merawat ikan hingga usia panen. Tiga hal yang paling penting dalam pemeliharaan budidaya ikan nila adalah pengelolaan air, pemberian pakan dan pengendalian hama penyakit. Dalam melakukan budi daya ikan nila ini, air menjadi hal yang sangat penting untuk kelangsungan hidup ikan tersebut. Guna mengoptimalkan air perlu diketahui kandungan isi air, seperti kadar oksigen dan PH air. Bisa juga dilakukan pemantauan kadar CO2, NH3 dan H2S bila memungkinkan. Jika kandungan oksigen di dalam kolam menurun, maka sirkulasi air lebih dikencangkan lagi.Jika kolam uda kotor dan ditandai dengan bau tak sedap, maka segeralah melakukan perbesihan kolam dengan cara menggantikan air. teknis nya, mula mula dibuang terlebih dahulu 2/3 air kotor dari kolam tersebut, kemudian dipenuhkan kembali dengan air bersih agar kolam kembali sehat dari hama penyakit.
+
+Pakan sangat penting untuk budidaya nila, dan juga menjadi biaya paling mahal dibandingkan dengan komponen lainnya. perlu diketahui, untuk pelet ikan nila berupa pelet dengan kadar protein 20-30%.
+
+Ikan nila membutuhkan pakan sebanyak 3% dari bobot tubuhnya setiap hari. Pemberian pakan bisa dilakukan pada pagi dan sore hari. Setiap dua minggu sekali, ambil sampel ikan nila secara acak kemudian timbang bobotnya. Lalu sesuaikan jumlah pakan yang harus diberikan.
+
+Perhitungan dosis pakan budidaya ikan nila:
+
+Dalam satu kolam terdapat 1500 ekor ikan nila berukuran 10-20 gram/ekor. Rata-rata bobot ikan → (10+20)/2 = 15 gram/ekor.
+
+Perhitungan pakannya → 15 x 1500 x 3% = 675 gram = 6,75 kg per hari Cek bobot ikan setiap dua minggu untuk menyesuaikan jumlah pakan.
+
+Seperti yang telah kita ketahui, ikan nila merupakan ikan yang tahan banting. Pada situasi normal, penyakit ikan nila tidak banyak mengkhawatirkan. Namun bila budidaya ikan nila sudah dilakukan secara intensif dan massal, resiko serangan penyakit harus diwaspadai.
+
+Penyebaran penyakit ikan sangat cepat, khususnya untuk jenis penyakit infeksi yang menular. Media penularan biasanya melewati air. Jadi bisa menjangkau satu atau lebih kawasan kolam. Untuk penjelasan lebih jauh silahkan baca hama dan penyakit ikan nila. Waktu yang diperlukan untuk budidaya ikan nila mulai dari penebaran benih hingga panen mengacu pada kebutuhan pasar. Ukuran ikan nila untuk pasar domestik berkisar 300-500 gram/ekor. Untuk memelihara ikan nila dari ukuran 10-20 gram hingga menjadi 300-500 gram dibutuhkan waktu sekitar 4-6 bulan.`;
