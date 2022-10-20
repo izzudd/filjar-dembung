1c1c328d4e622b7934ce13f1f29c98d3fbbfd37b
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2022-10-20 21:23:26 +0700

    update to nuxt 3 rc-12

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,25 @@
-import { defineNuxtConfig } from 'nuxt';
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/tw.css'],
 
+  routeRules: {
+    '/admin/**': { ssr: false },
+  },
+
   modules: ['@nuxtjs/tailwindcss'],
 
+  // set config with NUXT_<desired-config>
+  // convert camelCase to SCREAMING_SNAKE_CASE
+  // eg. NUXT_PUBLIC_BASE_URL
   runtimeConfig: {
     siteState: process.env.NODE_ENV,
     public: {
       apiEndpoint: '',
+      gaMeasurmentId: '',
     },
+  },
+
+  experimental: {
+    inlineSSRStyles: false,
   },
 });
